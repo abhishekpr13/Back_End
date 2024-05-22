407b5737378c5dd683887de10ffdb539afbe9e0c
Abhishek Prakash <156238051+abhishekpr13@users.noreply.github.com>
2024-05-22 19:30:45 -0400

    Final push for my 1st node.js project

--- Node-Frame/index.js
+++ Node-Frame/index.js
@@ -28,11 +28,13 @@ const dataObj = JSON.parse(data);
 
 
 const server = http.createServer((req,res)=>{
-    const pathName = req.url;
+  
+    const { query, pathname } = url.parse(req.url, true);
+
 
 
     //overview 
-    if(pathName ==='/'|| pathName === '/overview'){
+    if(pathname ==='/'|| pathname === '/overview'){
         res.writeHead(200, {'Content-type':'text/html'});
         const cardsHtml = dataObj.map(el=>replaceTemplate(tempCard,el)).join('');
         const output = tempOverview.replace('{%PRODUCT_CARDS%}',cardsHtml);
@@ -42,12 +44,16 @@ const server = http.createServer((req,res)=>{
 
         //Product page
 
-    } else if (pathName === '/product'){
-        res.end('This is the Product');
+    } else if (pathname === '/product'){
+        res.writeHead(200,{'Content-type':'text/html'});
+        const product = dataObj[query.id];
+        const output = replaceTemplate(tempProduct,product);
+        res.end(output)
 
+       
         //API
 
-    } else if(pathName === '/api'){
+    } else if(pathname === '/api'){
         res.writeHead(200,{'Content-type':'application/json'});
         res.end(data);
 
