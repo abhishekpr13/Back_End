355bab8109138c720504dcfac387bc056c755ac7
Abhishek Prakash <156238051+abhishekpr13@users.noreply.github.com>
2024-06-13 16:16:36 -0400

    Added post request handler

--- starter/app.js
+++ starter/app.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 const express = require('express')
 const app = express();
+app.use(express.json());
 
 
 const tours = JSON.parse(
@@ -16,6 +17,24 @@ app.get('/api/v1/tours',(req,res) =>{
             tours:tours
         }
     });
+});
+app.post('/api/v1/tours',(req,res)=>{
+    const newId = tours[tours.length-1].id+1;
+    const newTour = Object.assign({id:newId},req.body);
+    tours.push(newTour)
+    fs.writeFile(
+        '${__dirname}/dev-data/data/tours-simple.json',
+        JSON.stringify(tours),
+        err =>{
+            res.status(201)
+            .json({
+                status: 'success',
+                data: {
+                    tour : newTour
+                }
+            })
+        }
+    );
 });
 
 const port = 3000
