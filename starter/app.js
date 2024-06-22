9b16b399b7579ea32a3dca0fcfe82fab6694004c
Abhishek Prakash <156238051+abhishekpr13@users.noreply.github.com>
2024-06-22 19:24:47 -0400

    updating router

--- starter/app.js
+++ starter/app.js
@@ -7,9 +7,16 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes')
 
 const app = express();
-app.use(express.json());
+
+
 // Middelware
-app.use(morgan('dev'));
+if (process.env.NODE_ENV == 'development'){
+    app.use(morgan('dev'));
+}
+
+
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 app.use((req, res, next)=>{
     console.log('Hello from the middlewware');
     next();
