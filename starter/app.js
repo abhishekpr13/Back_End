da64ca74e385e0c34422222010bab997b5f20f49
Abhishek Prakash <156238051+abhishekpr13@users.noreply.github.com>
2024-06-23 02:55:03 -0400

    added prettier

--- starter/app.js
+++ starter/app.js
@@ -1,33 +1,31 @@
 const express = require('express');
 const morgan = require('morgan');
 
-
 const tourRouter = require('./routes/tourRoutes');
 
-const userRouter = require('./routes/userRoutes')
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-
 // Middelware
-if (process.env.NODE_ENV == 'development'){
-    app.use(morgan('dev'));
+// eslint-disable-next-line eqeqeq
+if (process.env.NODE_ENV == 'development') {
+  app.use(morgan('dev'));
 }
-
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
-app.use((req, res, next)=>{
-    console.log('Hello from the middlewware');
-    next();
+app.use((req, res, next) => {
+  console.log('Hello from the middlewware');
+  next();
 });
-app.use((req,res,next) =>{
-    req.requestTime = new Date().toString();
-    next()
+app.use((req, res, next) => {
+  req.requestTime = new Date().toString();
+  next();
 });
 
 //Route
-app.use('/api/v1/tours',tourRouter)
-app.use('/api/v1/users',userRouter)
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
 module.exports = app;
