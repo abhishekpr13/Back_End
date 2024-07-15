8fe864a2042dc910315314a6a6902a246968a170
Abhishek Prakash <156238051+abhishekpr13@users.noreply.github.com>
2024-07-15 02:49:03 -0400

    connected my express app with the database

--- starter/app.js
+++ starter/app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const morgan = require('morgan');
-const app = express();
+
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const app = express();
 
 //1)Middleware 
+if(process.env.NODE_ENV =='development'){
+    app.use(morgan('dev'));
+}
 app.use(morgan('dev'));
 
 app.use(express.json());
