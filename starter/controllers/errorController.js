f3cc8baebe70ba5640d1f13bea89167e93448af5
Abhishek Prakash <156238051+abhishekpr13@users.noreply.github.com>
2024-07-22 01:59:37 -0400

    implemented middleware to handle error

--- starter/controllers/errorController.js
+++ starter/controllers/errorController.js
@@ -1,9 +1,46 @@
+const sendErrorDev = (err, res) =>{
+    res.status(err.statusCode).json({
+        status: err.status,
+        error: err,
+        message: err.message,
+        stack: err.stack
+    });
+};
+
+const sendErrorProd = (err, res) =>{
+
+    // Operational error ,trusted error: send message to client
+    if (err.isOperational){
+        res.status(err.statusCode).json({
+            status: err.status,
+            message: err.message
+        }); 
+    // Programming or oter unknow error: don't leak erro details 
+    } else{
+        // 1) log erro 
+        console.error('Error',err);
+        //2) send message 
+        res.status(500).json({
+            status: 'error',
+            message: 'something went very wrong!'
+        })
+
+    }
+
+};
+
+
+
+
 module.exports = (err,req,res,next)=>{
     err.statusCode = err.statusCode || 500;
     err.status = err.status || 'error';
 
-    res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message
-    });
-}
+    if (process.env.NODE_ENV === 'development') {
+        sendErrorDev(err, res)
+    } else if (process.env.NODE_ENV === 'production'){
+        sendErrorProd(err,res)
+    }
+
+    
+};
