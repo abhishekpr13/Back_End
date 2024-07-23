af4d2f723b11510f912f958e047ef3fa4850187b
Abhishek Prakash <156238051+abhishekpr13@users.noreply.github.com>
2024-07-22 23:16:04 -0400

    Implemented error handler to handle prod and development error

--- starter/controllers/errorController.js
+++ starter/controllers/errorController.js
@@ -1,46 +1,73 @@
-const sendErrorDev = (err, res) =>{
+const AppError = require('./../utils/appError');
+
+const handleCastErrorDB = err => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = err => {
+  const value = err.message.match(/(["'])(\\?.)*?\1/)[0];
+  console.log(value);
+
+  const message = `Duplicate field value: ${value}. Please use another value!`;
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = err => {
+  const errors = Object.values(err.errors).map(el => el.message);
+
+  const message = `Invalid input data. ${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
+
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  // Operational, trusted error: send message to client
+  if (err.isOperational) {
     res.status(err.statusCode).json({
-        status: err.status,
-        error: err,
-        message: err.message,
-        stack: err.stack
+      status: err.status,
+      message: err.message
     });
+  } else {
+    // Programming or other unknown error: don't leak error details
+    console.error('ERROR 💥', err);
+
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong!'
+    });
+  }
 };
 
-const sendErrorProd = (err, res) =>{
+module.exports = (err, req, res, next) => {
+  // console.log(err.stack);
 
-    // Operational error ,trusted error: send message to client
-    if (err.isOperational){
-        res.status(err.statusCode).json({
-            status: err.status,
-            message: err.message
-        }); 
-    // Programming or oter unknow error: don't leak erro details 
-    } else{
-        // 1) log erro 
-        console.error('Error',err);
-        //2) send message 
-        res.status(500).json({
-            status: 'error',
-            message: 'something went very wrong!'
-        })
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
 
-    }
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    // Manually copy properties from the original error object
+    let error = Object.assign({}, err);
+    error.message = err.message;
+    error.name = err.name;
+    error.errmsg = err.errmsg;
+    error.errors = err.errors;
 
-};
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (error.name === 'ValidationError') 
+        error = handleValidationErrorDB(error);
 
-
-
-
-module.exports = (err,req,res,next)=>{
-    err.statusCode = err.statusCode || 500;
-    err.status = err.status || 'error';
-
-    if (process.env.NODE_ENV === 'development') {
-        sendErrorDev(err, res)
-    } else if (process.env.NODE_ENV === 'production'){
-        sendErrorProd(err,res)
-    }
-
-    
+    sendErrorProd(error, res);
+  }
 };
