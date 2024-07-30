c729e38fd1bb79bda61970a58b0ea93edde1038b
Abhishek Prakash <156238051+abhishekpr13@users.noreply.github.com>
2024-07-30 03:10:17 -0400

    Implemented Login functionality in my website

--- starter/controllers/userController.js
+++ starter/controllers/userController.js
@@ -1,9 +1,20 @@
-exports.getAllUsers = (req,res)=>{
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not implemented'
+const User = require('./../models/userModel');
+
+const catchAsync = require('./../utils/catchAsync');
+
+
+exports.getAllUsers = catchAsync(async (req,res,next)=>{
+    const users = await User.find();
+    
+    
+    res.status(200).json({
+        status: 'Success',
+        results:users.length,
+        data:{
+            users
+        }
     });
-};
+});
 
 exports.createUsers = (req,res)=>{
     res.status(500).json({
